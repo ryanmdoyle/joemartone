0550064263fa3fdd7bf6c6e8120e218bdfa3eeb4
Ryan Doyle <ryan@Ryans-iMac.local>
2019-12-03 21:21:34 -0800

    video gallery styles

--- web/src/pages/media.js
+++ web/src/pages/media.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { css } from '@emotion/core';
+import { useStaticQuery, graphql } from 'gatsby';
 
 import Layout from '../components/Layout';
 import PageTitle from '../components/PageTitle';
@@ -8,11 +10,61 @@ import AudioPlayer from '../components/AudioPlayer';
 // audio source
 import streamUrl from '../audio/oddity.mp3';
 
+const videoGallery = css`
+  display: flex;
+  flex-wrap: wrap;
+  box-sizing: border-box;
+  width: 100%;
+  .video {
+    box-sizing: border-box;
+    width: 100%;
+    padding: 5px;
+  }
+  
+  @media (min-width: 800px) {
+    .video {
+      width: calc(100% / 2 - 20px);
+      margin-left: 20px;
+    }
+    .video:first-child, .video:nth-child(4) {
+      width: 100%;
+    }
+  }
+`;
+
 const Media = () => {
+  const mediaQuery = useStaticQuery(graphql`
+    query mediaQuery {
+      allSanityMedia {
+        edges {
+          node {
+            mediaCategory
+            mediaList {
+              ... on SanityVideos {
+                id
+                video
+                videoCaption
+              }
+            }
+          }
+        }
+      }
+    }
+  `);
+  const videos = mediaQuery.allSanityMedia.edges[1].node.mediaList;
+  console.log(videos);
+
   return (
     <Layout>
       <PageTitle title='Media' />
-      <YoutubeVideo videoId='Qpy8dD7IrN0' />
+      <div css={videoGallery}>
+        {videos.map(video => (
+          <div className='video' key={video.id}>
+            {/* FIX LINK BELOW (currently uses link, needs to use id) */}
+            <YoutubeVideo videoId={video.video} />
+          </div>
+        ))}
+      </div>
       <AudioPlayer streamUrl={streamUrl} trackTitle='The track title and a longer title' preloadType='auto' />
     </Layout>
   );
