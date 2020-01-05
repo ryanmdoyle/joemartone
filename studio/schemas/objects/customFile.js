8fd23a06cbc72b782faa178f90693a75244eb859
Ryan Doyle <doylecodes@gmail.com>
2020-01-05 10:16:26 -0800

    make details easily accessible

--- studio/schemas/objects/customFile.js
+++ studio/schemas/objects/customFile.js
@@ -7,11 +7,17 @@ export default {
       title: 'Description',
       name: 'description',
       type: 'string',
+      options: {
+        isHighlighted: true // <-- make this field easily accessible, false makes hidden behin 'edit' button
+      },
     },
     {
       title: 'Author',
       name: 'author',
       type: 'string',
+      options: {
+        isHighlighted: true // <-- make this field easily accessible, false makes hidden behin 'edit' button
+      },
     }
   ]
 }
